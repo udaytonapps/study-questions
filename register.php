45af71cade2f57e227a43cd7718c8f55edbc801f
Andrew Niekamp <aniekamp1@udayton.edu>
2022-05-26 14:00:49 -0400

    TSUGI-308 Add images and update description

--- register.php
+++ register.php
@@ -4,7 +4,23 @@ $REGISTER_LTI2 = array(
     "name" => "Study Questions", // Name of the tool
     "FontAwesome" => "fa-graduation-cap", // Icon for the tool
     "short_name" => "Study Questions",
-    "description" => "This tool allows students to add study questions that other students in the course can see.", // Tool description
+    "description" => "Create a space for study questions to be added. Additional answers can be added to each study question. Students or instructors can add study questions and answers.", // Tool description
     "messages" => array("launch", "launch_grade"),
-    "tool_phase" => "new"
+    "tool_phase" => "new",
+    "license" => "Apache",
+    "languages" => array(
+        "English"
+    ),
+    "analytics" => array(
+        "internal"
+    ),
+    "video" => "https://udayton.warpwire.com/w/J_sEAA/",
+    // "source_url" => "SOURCE_URL",
+    "screen_shots" => array(
+        "images/Start.png",
+        "images/Study-Questions-Main-Page.png",
+        "images/Study-Question-Answer.png",
+        "images/Study-question-no-answer.png",
+        "images/Study-Guide-Contributions.png"
+    )
 );
